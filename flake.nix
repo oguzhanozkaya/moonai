3f1fcff5f80f3115a0d8baf946f697c88643af9a
oguzhanozkaya <ozkayaoguzhan67@gmail.com>
2026-04-09 13:48:16 +0300

    cuda path

--- flake.nix
+++ flake.nix
@@ -47,8 +47,7 @@
           CXX = "${pkgs.gcc}/bin/g++";
           VCPKG_ROOT = "${pkgs.vcpkg}/share/vcpkg";
           CUDA_PATH = "${pkgs.cudatoolkit}";
-          LD_LIBRARY_PATH = "${pkgs.cudatoolkit}/lib:${pkgs.lib.makeLibraryPath [ pkgs.libGL pkgs.libGLU ]}";
-
+          LD_LIBRARY_PATH = "/run/opengl-driver/lib:${pkgs.cudatoolkit}/lib:${pkgs.lib.makeLibraryPath [ pkgs.libGL pkgs.libGLU ]}";
           shellHook = ''
             echo "Project Packages and environment loaded for ${system}."
           '';
